b1eee4500fb5bf78b1f828a3bd0460e25fbe65dd
oguzhantomak <oguzhantomak@gmail.com>
2023-01-11 00:01:35 +0300

    Subscriber configurations added

--- RabbitMQExample.Publisher/Program.cs
+++ RabbitMQExample.Publisher/Program.cs
@@ -2,7 +2,7 @@
 using RabbitMQ.Client;
 
 var factory = new ConnectionFactory();
-factory.Uri = new Uri("yourMQConnectionString");
+factory.Uri = new Uri("amqps://ecgjjuwy:4sqODmsB-w4KJXi293xnQG5GzegqS2MG@sparrow.rmq.cloudamqp.com/ecgjjuwy");
 
 using var connection = factory.CreateConnection();
 var channel = connection.CreateModel();
@@ -13,7 +13,7 @@ var channel = connection.CreateModel();
  * exclusive = if you want to connect this que. from another channel/project you need to use false but if you want to only allow to connect this que in there you need tou mark as true
  * autodelete = if our subsc. is down you want to delete the que. you need to mark as true if you dont false.
  */
-channel.QueueDeclare("first queue ", true, false, false);
+channel.QueueDeclare("first queue", true, false, false);
 
 
 /*
--- RabbitMQExample.Subscriber/Program.cs
+++ RabbitMQExample.Subscriber/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("yourMQConnectionString");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+//channel.QueueDeclare("first queue ", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+/*
+ * consumer = que. name
+ * autoAck = if the message is true or false delete the message from que. If the mark false, if the message is true we need to approve the message it's true and then delete the message from que.
+ */
+channel.BasicConsume("first queue", true, consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine("Incoming message: " + message.ToString());
+};
+
+Console.ReadLine();
