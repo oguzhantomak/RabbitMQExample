5d3c04ef9b568b492c2ac9a7675456eec7a27f02
oguzhantomak <oguzhantomak@gmail.com>
2023-01-10 23:09:55 +0300

    Publish basic first queue

--- RabbitMQExample.Publisher/Program.cs
+++ RabbitMQExample.Publisher/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("yourMQConnectionString");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+/*
+ * queue = your que. name
+ * durable = if you restart rabbitmq your queue  still on.
+ * exclusive = if you want to connect this que. from another channel/project you need to use false but if you want to only allow to connect this que in there you need tou mark as true
+ * autodelete = if our subsc. is down you want to delete the que. you need to mark as true if you dont false.
+ */
+channel.QueueDeclare("first queue ", true, false, false);
+
+
+/*
+ * Messages are going to que. byte type.
+ */
+
+string message = "This is the first que.";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+/*
+ * If we dont use Exchange we need to provide routingKey same as declared queue
+ */
+
+channel.BasicPublish(string.Empty, "first queue", null, messageBody);
+Console.WriteLine("Message sent!");
+Console.ReadLine();
